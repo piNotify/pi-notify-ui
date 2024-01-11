62c57afd58f25fcc4f2dcaeecdb562a318cb6742
Eduardo Pilati <pilatieduardo@gmail.com>
2024-01-11 20:35:26 -0300

    intallation of google-fonts, i18n and bootstrap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,41 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    devtools: { enabled: true },
+    modules: [
+        '@nuxtjs/i18n',
+        '@nuxtjs/google-fonts',
+    ],
+    i18n:{
+        locales: [
+            {
+                code: 'en-US',
+                iso: 'en-US',
+                file: 'en-US.json',
+                name: 'English',
+            },
+            {
+                code: 'pt-BR',
+                iso: 'pt-BR',
+                file: 'pt-BR.json',
+                name: 'Português',
+            },
+        ],
+        defaultLocale: 'en-US',
+        langDir: 'lang',
+        lazy: true,
+        detectBrowserLanguage: {
+            useCookie: true,
+            cookieKey: 'i18n_language',
+        }
+    },
+    googleFonts: {
+        families: {
+            Poppins: true,
+            Lato: true,
+        },
+        display: 'swap',
+        download: true,
+        stylePath: 'assets/css/google-fonts.css',
+        fontsDir: 'assets/fonts',
+    }
 })
